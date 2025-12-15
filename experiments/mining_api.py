719d13a8af224b16dc5eee592146dc8ede4dd8a1
Sitotaw Ashagre <170337801+yotors@users.noreply.github.com>
2025-12-15 09:18:49 +0000

    Update: added a render.yaml file for ease of deployment

--- experiments/mining_api.py
+++ experiments/mining_api.py
@@ -1183,9 +1183,10 @@ if __name__ == '__main__':
     print()
     
     # Run the Flask app
+    port = int(os.environ.get('PORT', 5000))
     app.run(
         host='0.0.0.0',
-        port=5000,
-        debug=True,
+        port=port,
+        debug=os.environ.get('FLASK_DEBUG', 'True') == 'True',
         threaded=True
     )
